7cc4aff17d4af7eac8a81f3b0f7ca48f1847862b
birbwatcher <d@rkmind.cc>
2022-05-09 23:37:58 +0200

    refactor: comments deleted

--- virtual keyboard/script.js
+++ virtual keyboard/script.js
@@ -1,31 +1,3 @@
-// const keyCodes = [27, 96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 8, 9, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 20, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39 ,13 ,16, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, 38, 16, 17, 91, 18, 32, 37, 40, 39, 18, 17]
-// let row1 = [27, 96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 8];
-// let row2 = [9, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92]
-// let row3 = [20, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39 ,13]
-// let row4 = [16, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, 38, 16]
-// let row5 = [17, 91, 18, 32, 37, 40, 39, 18, 17]
-
-// function init() {
-//     let out = '';
-//     for (i=0; i<row1.length; i++) {
-//         if (i===15) {
-//             out += '<button class="backspace" data="'+ keyCodes[i] +'">'+String.fromCharCode(row1[i])+'</button>';
-//         };
-//         out += "<button data=" +keyCodes[i] +">"+String.fromCharCode(row1[i])+"</button>";
-//     }
-//     document.querySelector('.keyboard').innerHTML = out;
-// }
-
-// init();
-
-// document.onkeydown = function (event) {
-//     document.querySelector('[data="' +event.keyCode +'"]').classList.add("active");
-// }
-
-// document.onkeyup = function (event) {
-//     document.querySelector('[data="' +event.keyCode +'"]').classList.remove("active");
-// }
-
 const rowKey = ['Esc','`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
 const rowKey2 = ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'];
 const rowKey3 = ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter'];
@@ -38,7 +10,6 @@ const rowCode4 = ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', '
 const rowCode5 = ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'AltRight', 'ControlRight'];
 const serviceKeys = ['Escape', 'Backspace', 'Tab', 'CapsLock', 'Enter', 'ShiftLeft', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
 
-// var textInput = document.getElementById('keyboardTextArea');
 var textInput = document.createElement("textarea")
 var keyboard = document.createElement("div");
 var keyboardRow = document.createElement("div");
