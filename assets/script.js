5648dc05e71d83488fc079398bab4361730ed4ec
birbwatcher <d@rkmind.cc>
2022-08-16 21:30:44 +0200

    feat: exit on blackout click implementation

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
-document.querySelector('.burger').onclick = function () {
+let burger = document.querySelector('.burger');
+let blackout = document.querySelector('.blackout');
+
+burger.onclick = menuOpenClose;
+blackout.onclick = menuOpenClose;
+
+function menuOpenClose() {
     document.querySelector('.blackout').classList.toggle('modal-active');
     document.body.classList.toggle('noscroll');
     document.querySelector('.burger').classList.toggle('rotate');
     document.querySelector('.mobile-menu').classList.toggle('show');
 }
+
